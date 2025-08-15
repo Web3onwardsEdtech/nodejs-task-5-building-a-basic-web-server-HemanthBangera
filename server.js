23aac57302fea1e1873b6df4f1fface1a4a17176
HemanthBangera <hemanthbangera843@gmail.com>
2025-08-15 15:22:11 +0530

    Completed the task

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+// server.js
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to the Home Page');
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('This is the About page - Learn more about our Node.js web server');
+  } else if (req.url === '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Contact Us - Email: info@nodeserver.com | Phone: +1-555-123-4567');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
